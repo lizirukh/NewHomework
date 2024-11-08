3a71ee799342702223e394d15edb7ede9fd987bb
lizirukh <lizirukh@gmail.com>
2024-11-09 03:11:22 +0400

    updated homework file

--- Lecture16/homework.py
+++ Lecture16/homework.py
@@ -33,21 +33,46 @@
 # print(functor(300))
 
 # დავალება 3
-import time
+# import time
 
-def decorator(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        func()
-        return "--- %s seconds ---" % (time.time() - start_time)
-    return wrapper
+# def decorator(func):
+#     def wrapper(*args, **kwargs):
+#         start_time = time.time()
+#         func()
+#         return "--- %s seconds ---" % (time.time() - start_time)
+#     return wrapper
 
-@decorator 
-def print_time():
-    x = 0
-    while x < 100:
-        print(f'X is {x} and is less than 10')
-        x+=2
+# @decorator 
+# def print_time():
+#     x = 0
+#     while x < 100:
+#         print(f'X is {x} and is less than 10')
+#         x+=2
 
+# print(print_time())
 
-print(print_time())
+# დავალება 4
+class LoggingMeta(type):
+    def __new__(self, cls, bases, dct):
+        
+        instance = super().__new__(self, cls, bases, dct)
+        print(f'The class name is {cls} and methods are: {dct}')
+        return instance  
+    
+# print(type(LoggingMeta))
+class Dog(metaclass=LoggingMeta):
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    
+    def show_info(self):
+        return f'{self.name} - {self.breed}'
+
+    
+dog1 = Dog('Jay', 'English Cocker Spaniel')
+dog2 = Dog('Tulio', 'Siberian Husky')
+dog3 = Dog("Molly", 'English Lab')
+
+print(dog1.show_info())
+print(dog2.show_info())
+print(dog3.show_info())
