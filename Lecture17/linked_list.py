36a24eed3278445e2e19ab9c44491d30a670b996
lizirukh <lizirukh@gmail.com>
2024-11-11 02:28:33 +0400

    updated file

--- Lecture17/linked_list.py
+++ Lecture17/linked_list.py
@@ -12,34 +12,78 @@ class LinkedList:
         new_node = Node(data) # ვქმნით ახალ Node-ის ობიექტს 
         if self.head is None: ## თუ head-ი ცარიელია,  
             self.head = new_node ## head-ს ვანიჭებთ ახალ შექმნილ ობიექტს
-            return # და უბრალოდ ვწერთ return-ს რომ გამოვიდეს if-იდან 
+            return # და უბრალოდ ვწერთ return-ს რომ გამოვიდეს append მეთოდიდან
         
         last_node = self.head  # არ ვიცით ბოლო ელემენტი რა არის, ამიტომ last_node-ს ვანიჭებთ head-ს
         while last_node.next: # სანამ last_node-ის next-ი none არ არის
             last_node = last_node.next # last_node-ს ვანიჭებთ last_node-ის next-ს
         last_node.next = new_node # ესე ვპოულობთ ბოლო ელემენტს, რომლის next-იც ცარიელია ანუ none-ია, და იქ ვამატებთ new node-ს
 
-    def remove_at(self, index):
-        if index < 0 or self.head is None:
+    def remove_at(self, index): 
+        if index < 0 or self.head is None:  # თუ ინდექსი არის ნოლზე ნაკლები ან head არის ცარიელი 
+            return # return-ით ვაჩერებთ მეთოდს რომ მის მერე არსებული ლოგიკა აღარ ამოქმედდეს
+        
+        if index == 0: # განვიხილავთ იმ ვარიანტს რომელშიც ინდექსი ნულია
+            self.head = self.head.next # წაშლის მაგივრად ხდება გადაწერა linked list-ებში, და პირველ ინდექსზე მყოფ ელემენტს უბრალოდ ვანიჭებთ შემდეგი ელემენტის მონაცემს
+            return 
+        
+# მაშინ როცა ინდექსი ნოლზე მეტია და გვჭიდება მოძებნა ელემენტის, ინდექსის მიხედვით, საწყის ელემენტად ვირჩევთ ჰედს. და საწყის ინდექსის პოზიციად ნოლს. 
+        current_node = self.head
+        current_position = 0
+# while ციკლი უნდა გაჩერდეს მაშინ როცა გადაცემული ინდექსის ელემენტს აქვს ნექსთი, და ინდექსის პოზიცია ნაკლებია 
+# index - 1-ზე, რადგან უნდა ვიყოთ წინა ელემენტზე შემდეგი ელემენტის ამოსაშლელად
+        while current_node.next and current_position < index - 1:
+            current_node = current_node.next # უნდა ვცვალოთ ამ ცვლადის მნიშვნელობა რომ ციკლმა გააგრძელოს მოქმედება
+            current_position += 1 # index-ის პოზიციაც უნდა იზრდებოდეს ერთით ყოველ ჯერზე
+
+# თუ ინდექსი სწორად არის გადაცემული, ვდგებით წინა ელემენტზე და შემდეგს ვანიჭებთ მის შემდეგ ელემენტს
+        if current_node.next:
+            current_node.next = current_node.next.next
+            
+
+    def insert_at(self, data, index):
+        if index < 0:
             return 
         
         if index == 0:
-            self.head = self.head.next
-            return 
+            self.head.next = self.head
+            self.head = data
+            return
+        
+        current_node = self.head
+        current_position = 0
+
+        while current_position < index:
+            current_node = current_node.next
+            current_position += 1
+
+        current_node = data
 
     def display(self):
-        current_node = self.head
-        while current_node:
-            print(current_node.data, end=' -> ')
-            current_node = current_node.next
+        current_node = self.head # გამოსატანად შემოგვაქვს current_node ცვლადი, და ვანიჭებთ self.head-ს რადგან დაიბეჭდოს ბოლო ელემენტიც 
+        while current_node: # სანამ current_node არსებობს..
+            print(current_node.data, end=' -> ') # იქამდე გამოიტანოს current_node-ის მონაცემი და default endline-ს ვცვლით ისრით, ქვედა ხაზზე ჩამოტანის მაგივრად (რომელიც გათვალისწინებულია default-ად)
+            current_node = current_node.next # current_node-ს ვანიჭებთ შემდეგი ელემენტის data-ს ანუ current_node.next-ს რომ არ მივიღოთ დაუსრულებელი ციკლი 
 
-linked_list = LinkedList()
+        print() # ვიძახებთ ცარიელ პრინტს რომ შეცვლილი endline დაუბრუნდეს თავის დეფოლტ მნიშვნელობას
+linked_list = LinkedList() # ვქმნით ობიექტს რომ შევძლოთ მეთოდების გამოყენება
 
-linked_list.append(50)
-linked_list.append(87)
-linked_list.append(20)
-linked_list.append(25)
-linked_list.append(11)
-linked_list.append(5)
+linked_list.append(50) # 0 # ვამატებთ ამ მონაცემებს
+linked_list.append(87) # 1
+linked_list.append(20) # 2
+linked_list.append(25) # 3
+linked_list.append(11) # 4
+linked_list.append(5)  # 5
 
+linked_list.display() # display მეთოდის გამოყენებით გამოგვაქვს მონაცემები 
+# linked_list.remove_at(2) 
+# linked_list.display()
+# linked_list.remove_at(2) 
+# linked_list.display()
+linked_list.remove_at(0) 
+linked_list.display()
+linked_list.remove_at(4)
+linked_list.display()
+
+linked_list.insert_at(15, 0)
 linked_list.display()
